1c66143a4f45554ba84f89c978cea170664f8a5b
sgw-ryota <ryota-segawe@sekawamukufutoshinoMacBook-Pro.local>
2021-07-10 20:19:31 +0900

    edit 404page

--- 404.php
+++ 404.php
@@ -1,1 +1,9 @@
-404page
+<?php get_header();?>
+<div class="container height-max">
+    <div class="row ptb-200">
+        <p class="text-center">404</p>
+        <p class="text-center">ページが見つかりません。</p>
+        <p class="text-center"><a href="<?php echo home_url(); ?>">トップへ戻る</a></p>
+    </div>
+</div>
+<?php get_footer();?>
